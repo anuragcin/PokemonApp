efcc9f0913f9096501144819e76aee2e64c228f2
Anurag <anurag@Tamannas-MacBook-Pro.local>
2019-12-19 18:04:43 -0800

    Updates

---
+++ precache-manifest.1ad50d39cf15099505fd4ba5ea7dee58.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "889f2da8845cd77bd968",
+    "url": "/PokemonApp/static/css/main.01107189.chunk.css"
+  },
+  {
+    "revision": "889f2da8845cd77bd968",
+    "url": "/PokemonApp/static/js/main.889f2da8.chunk.js"
+  },
+  {
+    "revision": "f561dd0c9e97c43b094d",
+    "url": "/PokemonApp/static/js/1.f561dd0c.chunk.js"
+  },
+  {
+    "revision": "4dbe965c533a66ecd641",
+    "url": "/PokemonApp/static/js/runtime~main.4dbe965c.js"
+  },
+  {
+    "revision": "53a949adf94caad88450120f7f17bdf6",
+    "url": "/PokemonApp/index.html"
+  }
+];
